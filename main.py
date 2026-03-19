84295e40a79649264023efa9e6f094cf10bb062c
islamaslanov883-coder <islamaslanov883@gmail.com>
2026-03-19 21:05:14 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+# a) Input & Variables
+name = input("Enter student name: ")
+math = float(input("Enter Math grade: "))
+physics = float(input("Enter Physics grade: "))
+python_grade = float(input("Enter Python grade: "))
+
+# b) Calculations
+average = (math + physics + python_grade) / 3
+scholarship = 35000 if average >= 90 else 0
+gpa = average / 25
+
+# c) Formatted Output
+print("=" * 30)
+print("    STUDENT REPORT CARD")
+print("=" * 30)
+
+print("Student :", name)
+print("Math :", math)
+print("Physics :", physics)
+print("Python :", python_grade)
+
+print("-" * 30)
+
+print("Average :", round(average, 2))
+print("GPA :", round(gpa, 2))
+print("Scholarship :", scholarship, "KZT")
+
+print("=" * 30)
+
+# d) Comparison
+print("Scholarship granted:", average >= 90)
+print("Perfect score:", average == 100)
